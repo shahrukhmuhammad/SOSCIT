6a18711d3d2020daac4ae95cfaf6d6c85e2d40ef
sherikhan <sherikhanx@gmail.com>
2020-01-06 10:54:29 +0500

    accesstoken-commented

--- webapp/APIController/AccountController.cs
+++ webapp/APIController/AccountController.cs
@@ -22,100 +22,102 @@ namespace WebApp.APIController
             vehicleRepo = new VehicleEntity();
             orderBookingRepo = new OrderbookingEntity();
         }
-        string AccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1NjIiLCJqdGkiOiJkMjhhMTQ4ZC01MDJkLTQyYWUtYWUxMC03MWExM2Y4MmRmYTYiLCJ1c2VyLWlkIjoiNTYyIiwiZnVsbC1uYW1lIjoiQWRtaW5UZXN0IE1hemF5YVRlc3QiLCJlbWFpbC1hZGRyZXNzIjoiYWRtaW5AbWF6YXlhLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNTcxMzE5ODc0LCJpc3MiOiJodHRwOi8vbWF6YXlhLWFwaS1xYS5henVyZXdlYnNpdGVzLm5ldCIsImF1ZCI6Imh0dHA6Ly9tYXpheWEtYXBpLXFhLmF6dXJld2Vic2l0ZXMubmV0In0.ROh_7the6UJj0V_Y5mU1eeGbqz1X2SO2ZYmclBvlbVM";
+        //string AccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1NjIiLCJqdGkiOiJkMjhhMTQ4ZC01MDJkLTQyYWUtYWUxMC03MWExM2Y4MmRmYTYiLCJ1c2VyLWlkIjoiNTYyIiwiZnVsbC1uYW1lIjoiQWRtaW5UZXN0IE1hemF5YVRlc3QiLCJlbWFpbC1hZGRyZXNzIjoiYWRtaW5AbWF6YXlhLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNTcxMzE5ODc0LCJpc3MiOiJodHRwOi8vbWF6YXlhLWFwaS1xYS5henVyZXdlYnNpdGVzLm5ldCIsImF1ZCI6Imh0dHA6Ly9tYXpheWEtYXBpLXFhLmF6dXJld2Vic2l0ZXMubmV0In0.ROh_7the6UJj0V_Y5mU1eeGbqz1X2SO2ZYmclBvlbVM";
 
         [HttpPost]
         //[Route("api/Account/Login")]
         public IHttpActionResult Login([FromBody]VehicleLoginModel objUser)
         {
-            var accesstoken = Request.Headers.GetValues("Authorization").FirstOrDefault();
-
-            if (AccessToken == accesstoken)
+            //var accesstoken = Request.Headers.GetValues("Authorization").FirstOrDefault();
+            //if (AccessToken == accesstoken)
+            //{
+            #region Login
+            if (!string.IsNullOrEmpty(objUser.vehicleNumber) && !string.IsNullOrEmpty(objUser.password))
             {
-                #region Login
-                if (!string.IsNullOrEmpty(objUser.vehicleNumber) && !string.IsNullOrEmpty(objUser.password))
+                var res = vehicleRepo.AuthnticateVehicle(objUser);
+                var orderDetails = new Vew_Orderbookings();
+                if (res != null && res == "Login Successfully!")
                 {
-                    var res = vehicleRepo.AuthnticateVehicle(objUser);
-                    var orderDetails = new Vew_Orderbookings();
-                    if (res != null && res == "Login Successfully!")
-                    {
-                        return Json(new { error = "false", message = "Login Success!" });
-
-                        //try
-                        //{
-                        //    //orderDetails = orderBookingRepo.GetOrderbyVehicleNumber(objUser.vehicleNumber);
-                        //    return Json(new { error = "false", message = "Login Success!" });
-                        //}
-                        //catch (Exception ex)
-                        //{
-                        //    return Json(new { error = "true", message = ex.Message.ToString(), });
-                        //}
-                    }
-                    else
-                        return Json(new { error = "true", message = "Failed! User Does not exist" });
+                    return Json(new { error = "false", message = "Login Success!" });
+                    //try
+                    //{
+                    //    //orderDetails = orderBookingRepo.GetOrderbyVehicleNumber(objUser.vehicleNumber);
+                    //    return Json(new { error = "false", message = "Login Success!" });
+                    //}
+                    //catch (Exception ex)
+                    //{
+                    //    return Json(new { error = "true", message = ex.Message.ToString(), });
+                    //}
                 }
-                #endregion
+                else
+                    return Json(new { error = "true", message = "Failed! Vehicle/User Does not exist" });
             }
-            return Json(new { error = "true", message = "Error! Provided AccessToken is not correct" });
+            #endregion
+            return Json(new { error = "true", message = "Vehicle Number Can't be Empty!" });
+
+            // }
+            /// return Json(new { error = "true", message = "Error! Provided AccessToken is not correct" });
         }
 
         [HttpPost]
         //[Route("api/Account/GetOrderDetails")]
         public IHttpActionResult GetOrderDetails([FromBody]VehicleLoginModel objUser)
         {
-            var accesstoken = Request.Headers.GetValues("Authorization").FirstOrDefault();
-            if (AccessToken == accesstoken)
+            //var accesstoken = Request.Headers.GetValues("Authorization").FirstOrDefault();
+            //if (AccessToken == accesstoken)
+            //{
+            #region Login
+            if (!string.IsNullOrEmpty(objUser.vehicleNumber))
             {
-                #region Login
-                if (!string.IsNullOrEmpty(objUser.vehicleNumber))
+                var orderDetails = new Vew_Orderbookings();
+                try
                 {
-                    var orderDetails = new Vew_Orderbookings();
-                    try
-                    {
-                        orderDetails = orderBookingRepo.GetOrderbyVehicleNumber(objUser.vehicleNumber);
+                    orderDetails = orderBookingRepo.GetOrderbyVehicleNumber(objUser.vehicleNumber);
 
-                        if (orderDetails != null)
+                    if (orderDetails != null)
+                    {
+                        var crewList = vehicleRepo.GetCrewListByVehicleId(orderDetails.VehicleId.Value);
+
+                        return Json(new
                         {
-                            var crewList = vehicleRepo.GetCrewListByVehicleId(orderDetails.VehicleId.Value);
-
-                            return Json(new
+                            error = "false",
+                            message = "Order Exist",
+                            details = new
                             {
-                                error = "false",
-                                message = "Order Exist",
-                                details = new
-                                {
-                                    OrderId = orderDetails.Id,
-                                    orderDetails.OrderNo,
-                                    orderDetails.VehicleNumber,
-                                    orderDetails.Date,
-                                    orderDetails.ApprovedByName,
-                                    orderDetails.ApprovedOn,
-                                    orderDetails.CollectionBranchName,
-                                    orderDetails.DeliveryBranchName,
-                                    orderDetails.BilledBranchName,
-                                    orderDetails.Status,
-                                    StatusTitle = Convert.ToString((AnnexureStatus)orderDetails.Status),
-                                    OrderType = Convert.ToString((OrderType)orderDetails.OrderType),
-                                    TransportationMood = Convert.ToString((TransportationMood)orderDetails.TransportationMood).ToSpacedTitleCase(),
-                                },
-                                crewList = crewList.CITVehicleCrews.Select(x => new {
-                                    x.EmployeeId,
-                                    EmployeeName = x.CPCEmployee.Code + " " + x.CPCEmployee.Name,
-                                    x.CPCEmployee.CNIC
-                                }).ToList(),
-                            });
-                        }
-                        else
-                            return Json(new { error = "true", message = "No Order Found" });
+                                OrderId = orderDetails.Id,
+                                orderDetails.OrderNo,
+                                orderDetails.VehicleNumber,
+                                orderDetails.Date,
+                                orderDetails.ApprovedByName,
+                                orderDetails.ApprovedOn,
+                                orderDetails.CollectionBranchName,
+                                orderDetails.DeliveryBranchName,
+                                orderDetails.BilledBranchName,
+                                orderDetails.Status,
+                                StatusTitle = Convert.ToString((AnnexureStatus)orderDetails.Status),
+                                OrderType = Convert.ToString((OrderType)orderDetails.OrderType),
+                                TransportationMood = Convert.ToString((TransportationMood)orderDetails.TransportationMood).ToSpacedTitleCase(),
+                            },
+                            crewList = crewList.CITVehicleCrews.Select(x => new
+                            {
+                                x.EmployeeId,
+                                EmployeeName = x.CPCEmployee.Code + " " + x.CPCEmployee.Name,
+                                x.CPCEmployee.CNIC
+                            }).ToList(),
+                        });
                     }
-                    catch (Exception ex)
-                    {
-                        return Json(new { error = "true", message = ex.Message.ToString() });
-                    }
+                    else
+                        return Json(new { error = "true", message = "No Order Found" });
                 }
-                #endregion
+                catch (Exception ex)
+                {
+                    return Json(new { error = "true", message = ex.Message.ToString() });
+                }
             }
-            return Json(new { error = "true", message = "Error! Provided AccessToken is not correct" });
+            return Json(new { error = "true", message = "Vehicle Number Can't be Empty!" });
+            #endregion
+            //}
+            //return Json(new { error = "true", message = "Error! Provided AccessToken is not correct" });
         }
 
     }
